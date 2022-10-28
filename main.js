a5a072f75e88fb3245776202e6d403788547eb3e
via172729 <43636180+via172729@users.noreply.github.com>
2022-10-28 18:50:01 +0800

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-var animation = bodymovin.loadAnimation({
-	container: document.getElementById('bm'), // Required
-	path: 'animatetest.json', // Required
-	renderer: 'svg/canvas/html', // Required
-	loop: true, // Optional
-	autoplay: true, // Optional
-	name: "Hello World", // Name for future reference. Optional.
-  })
